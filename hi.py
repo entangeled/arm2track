a2d37a18663e7ac304d727035da6312a8ca7df60
Entangeled <ergaarder@gmail.com>
2025-09-30 18:33:44 +1000

    a skeleton code for robot all 3

--- hi.py
+++ hi.py
@@ -1,6 +1,32 @@
 #h
-import math 
+from math import pi 
 from ir_support import LinearUR3
 
 
 robot = LinearUR3()
+
+
+class Robot1:
+    def __init__(self):
+        self.robot = LinearUR3() #need to change to the right robot name
+
+class Robot2:
+    def __init__(self):
+        self.robot = LinearUR3() # need to change to the right robot name
+
+class Robot3:
+    def __init__(self):
+        self.robot = LinearUR3() # need to change to the right robot name
+
+class Robot4:
+    def __init__(self):
+        self.robot = LinearUR3() # need to change to the right robot name   
+
+
+def __init__(self, congig: dict):
+    self.config = config
+    self.robot1 = Robot1()
+    self.robot2 = Robot2()
+    self.robot3 = Robot3(
+
+    )
